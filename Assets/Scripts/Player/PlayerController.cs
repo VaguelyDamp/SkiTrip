0c84167af99d2961fa84cf995f82e56f3d22f14a
Eli <elijw22@gmail.com>
2021-04-24 00:57:47 -0400

    trying the new input system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,18 +7,20 @@ public class PlayerController : MonoBehaviour
     public CharacterController characterController;
     public float speed = 5;
 
+    PlayerInputs inputActions;
+
     void Start()
     {
-        
+        inputActions = new PlayerInputs(); 
     }
 
     void Update()
     {
-        
+        Move();
     }
 
     private void Move()
     {
-
+        Debug.Log(inputActions.Steering.Move.ReadValue<float>());
     }
 }
