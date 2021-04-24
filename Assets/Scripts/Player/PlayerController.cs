d3b4d9bd5f32dc78314f6a3501ba2bb7eb03df15
cmeese549 <chris@meesewebdesigns.com>
2021-04-23 23:28:58 -0600

    Setup input system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController characterController;
     public float speed = 5;
 
-    PlayerInputs inputActions;
+    public delegate void SteerEvent();
 
-    void Start()
+    //OnSteer is automatically a thing because we have 
+    //an action called "steer" in the input map thing
+    //everything changed recently idk how to make it like
+    //it is in divine drifter but this should work better
+    //becuase you only ever subscribe to inputs in the
+    //player controller and then subscribe to subsequent
+    //events that we call in other scripts to handle the input
+    public event SteerEvent Steer;
+    void OnSteer(InputValue value)
     {
-        inputActions = new PlayerInputs(); 
+        Steer?.Invoke();
+        Move(value);
     }
 
-    void Update()
+    private void Move(InputValue value)
     {
-        Move();
-    }
-
-    private void Move()
-    {
-        Debug.Log(inputActions.Steering.Move.ReadValue<float>());
+       Debug.Log(value.Get<float>());
     }
 }
