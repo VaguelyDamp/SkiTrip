66c8b06af76cc39600cb9dd5fce5e3f0d4f3d165
Eli <elijw22@gmail.com>
2021-04-24 00:01:52 -0400

    why does git have to be so fucking dumb

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+
+    }
+}
