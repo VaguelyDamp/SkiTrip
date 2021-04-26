208b21204baa884b4ccb9bca63c52a047ca92e0c
Eli <elijw22@gmail.com>
2021-04-25 20:38:11 -0400

    ramps better

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     private GameController gameController;
+    private PlayerController playerController;
+
+    public bool ramping = false;
 
     private void Start()
     {
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        playerController = gameObject.GetComponent<PlayerController>();
     }
 
     private void OnCollisionEnter(Collision col)
@@ -19,6 +23,31 @@ public class PlayerCollision : MonoBehaviour
             col.transform.parent?.transform.parent.tag == "Prop")
         {
             gameController.Death();
+        }
+
+        if(col.transform.tag == "Ramp")
+        {
+            //ramping = true;
+            //Debug.Log("Start Ramp");
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.transform.name == "ForceGate")
+        {
+            //ramping = false;
+            Debug.Log("Applying ramp force");
+            playerController.applyRamp = 1;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.name == "ForceGate")
+        {
+            //ramping = false;
+            Debug.Log("Applying ramp force (trigger)");
+            playerController.applyRamp = 1;
         }
     }
 }
