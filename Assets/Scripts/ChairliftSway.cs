f8060c2fac2c3653c197ca980116cb8119c0111e
Eli <elijw22@gmail.com>
2021-04-26 02:15:33 -0400

    chairlift sway script

---
+++ Assets/Scripts/ChairliftSway.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairliftSway : MonoBehaviour
+{
+    public float swayAmount = 3;
+    public float swaySpeed = .2f;
+
+    private float sway = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sway >=0)
+        {
+            sway = Mathf.MoveTowardsAngle(sway, -swayAmount, swaySpeed);
+        }
+        else
+        {
+            sway = Mathf.MoveTowardsAngle(sway, swayAmount, swaySpeed);
+        }
+
+        transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, sway);
+    }
+}
