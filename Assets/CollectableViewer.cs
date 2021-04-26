ac48e3b44b499f4bc9a8ae93cab98cfb6d8375cd
cmeese549 <chris@meesewebdesigns.com>
2021-04-26 07:59:44 -0600

    Peni

---
+++ Assets/CollectableViewer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableViewer : MonoBehaviour
+{
+    private string whale = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ShowCollectables ()
+    {
+
+    }
+}
